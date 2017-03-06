f7aa352dbfe36c861a99425922efecaf72fccb4b
Lauro Chicorski <laurocoelhoc@gmail.com>
2017-03-06 12:54:48 -0300

    Implementando

---
+++ src/main/resources/static/js/docrotasApp.js
@@ -0,0 +1,51 @@
+angular.module('docrotasApp', [])
+    .controller('UfCtrl', ['$http',function ($http) {
+        var self = this;
+        self.ufs = [];
+        self.uf = {};
+        self.filtro = {};
+
+        self.buscarTodos = function () {
+            return $http.get('uf/').then(
+                function (response) {
+                    self.ufs = response.data;
+                }, function (errResponse) {
+                    console.error('Erro');
+                });
+        };
+
+        self.editar = function (id) {
+        	return $http.get('uf?id=' + id).then(
+        		function (response) {
+        			self.uf = response.data[0];
+        		}, function(errReponse) {
+					console.error('Erro');
+				});
+        }
+        self.salvar = function () {
+            $http.post('uf/', self.uf)
+                .then(function sucesso (response) {
+                	self.buscarTodos();
+                	self.novo();
+                })
+                .then(function(response) {
+                    console.log(response);
+                });
+        };
+
+        self.novo = function () {
+            self.uf = {};
+        }
+
+        self.excluir = function (id) {
+            $http.delete('uf/' + id)
+                .then(function () {
+                    self.buscarTodos();
+                })
+                .then(function (response) {
+                    self.novo();
+                })
+        }
+
+        self.buscarTodos();
+    }]);
