150c7b3b626282743dec0d85a436af2c2e5a6b56
Lauro Chicorski <laurocoelhoc@gmail.com>
2017-03-30 01:46:16 -0300

    Implementando paginação

--- src/main/resources/static/js/docrotasApp.js
+++ src/main/resources/static/js/docrotasApp.js
@@ -1,4 +1,4 @@
-var app = angular.module('docrotasApp', ['ngRoute']);
+var app = angular.module('docrotasApp', ['ngRoute', 'ui.bootstrap','ngAnimate', 'ngSanitize']);
 
 app.config(['$routeProvider', function($routeProvider){
 	$routeProvider.when('/uf',{
@@ -12,29 +12,60 @@ app.config(['$routeProvider', function($routeProvider){
     })
 }])
 
-app.controller('UfCtrl', ['$http',function ($http, $rootScope) {
+app.controller('UfCtrl', ['$http',function ($http, $rootScope, $log) {
     var self = this;
     self.ufs = [];
     self.uf = {};
     self.filtro = {};
+    var qtd = 15;
+    self.modoGrade = true;
+    self.modoFormulario = false;
+
+    self.tamanhoMax = 15;
+    self.totalItens = 1;
+    self.paginaAtual = 1;
+    self.numPaginas = 1;
+
+    self.pageChanged = function() {
+         $log.log('Page changed to: ' + self.currentPage);
+    };
+
+    self.habilitarModoGrade = function () {
+        self.modoGrade = true;
+        self.modoFormulario = false;
+    }
+
+    self.habilitarModoFormulario = function () {
+        self.modoGrade = false;
+        self.modoFormulario = true;
+    }
 
     self.buscarTodos = function () {
-        return $http.get('uf?pagina=0&qtd=20').then(
+        var pagina = self.paginaAtual - 1;
+        return $http.get('uf?pagina=' + pagina + '&qtd=' + self.tamanhoMax).then(
             function (response) {
                 self.ufs = response.data.content;
+                self.paginaAtual = response.data.number + 1;
+                self.totalItens = response.data.totalElements;
+                self.numPaginas = response.data.totalPages;
             }, function (errResponse) {
                 console.error('Erro');
             });
     };
 
     self.buscarPorId = function (id) {
-        return $http.get('uf?id=' + id + '&pagina=0&qtd=20').then(
+        return $http.get('uf?id=' + id + '&pagina=0&qtd=' + qtd).then(
             function (response) {
                 self.uf = response.data.content[0];
             }, function(errReponse) {
                 console.error('Erro');
             });
     };
+
+    self.editar = function (id) {
+        self.habilitarModoFormulario();
+        self.buscarPorId(id);
+    }
 
     self.salvar = function () {
         $http.post('uf/', self.uf)
@@ -47,6 +78,7 @@ app.controller('UfCtrl', ['$http',function ($http, $rootScope) {
     };
 
     self.novo = function () {
+        self.habilitarModoFormulario();
         self.uf = {};
     };
 
