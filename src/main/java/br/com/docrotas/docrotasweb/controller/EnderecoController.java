c631fa73221ed64028c60c023d59e50ddf6a2134
Unknown <laurocoelhoc@gmail.com>
2017-06-01 13:21:59 -0300

    Implementação tela cadastro de Pessoa

--- src/main/java/br/com/docrotas/docrotasweb/controller/EnderecoController.java
+++ src/main/java/br/com/docrotas/docrotasweb/controller/EnderecoController.java
@@ -4,20 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.docrotas.docrotasweb.entity.Empresa;
 import br.com.docrotas.docrotasweb.entity.Endereco;
-import br.com.docrotas.docrotasweb.entity.EnderecoID;
-import br.com.docrotas.docrotasweb.entity.TipoEndereco;
 import br.com.docrotas.docrotasweb.repository.EnderecoRepository;
-import br.com.docrotas.docrotasweb.repository.PessoaRepository;
 
 @RestController
 public class EnderecoController {
--- src/main/java/br/com/docrotas/docrotasweb/entity/TipoEndereco.java
+++ src/main/java/br/com/docrotas/docrotasweb/entity/TipoEndereco.java
@@ -1,9 +1,16 @@
 package br.com.docrotas.docrotasweb.entity;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum TipoEndereco {
 	
-	PRINCIPAL,
-	COBRANCA,
-	ENTREGA;
+	PRINCIPAL, 	// 0
+	COBRANCA,	// 1
+	ENTREGA;	// 2
+	
+	@JsonValue
+	public int toValue() {
+		return ordinal();
+	}
 
 }
