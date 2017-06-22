a13b96ece6650495e61d907847a3c416bfdb6c08
Unknown <laurocoelhoc@gmail.com>
2017-06-22 11:50:06 -0300

    10018: Auto stash before merge of "10018" and "origin/10018"

--- src/main/java/br/com/docrotas/docrotasweb/entity/TipoCTe.java
+++ src/main/java/br/com/docrotas/docrotasweb/entity/TipoCTe.java
@@ -6,7 +6,7 @@ public enum TipoCTe {
 	NORMAL("0"),
 	COMPLEMENTAR("1"),
 	ANULACAO("2"),
-	SUBSTITITUO("3");
+	SUBSTITUTO("3");
 	
 	private String codigo;
 	
--- src/main/resources/static/js/cteController.js
+++ src/main/resources/static/js/cteController.js
@@ -6,6 +6,34 @@ angular.module('docrotasApp').controller('CteCtrl', function ($http, $rootScope,
     self.cte = {};
     self.ctes = [];
 
+    self.tpEmissaoOptions = [
+        {id : 0, descricao : 'Normal'},
+        {id : 1, descricao : 'EPEC SVC'},
+        {id : 2, descricao : 'Contigência FSDA'},
+        {id : 3, descricao : 'SVC-RS'},
+        {id : 4, descricao : 'SVC-SP'}
+    ];
+
+    self.tpAmbienteOptions = [
+        {id : 0, descricao : 'Produção'},
+        {id : 1, descricao : 'Homologação'}
+    ];
+
+    self.tpServicoOptions = [
+        {id : 0, descricao : 'Normal'},
+        {id : 1, descricao : 'Subcontratação'},
+        {id : 2, descricao : 'Resdepacho'},
+        {id : 3, descricao : 'Resdepacho Intermediário'},
+        {id : 4, descricao : 'Serviço vinculado multimodal'}
+    ];
+
+    self.tpCTeOptions = [
+        {id : 0, descricao : 'Normal'},
+        {id : 1, descricao : 'Complementar'},
+        {id : 2, descricao : 'Anulação'},
+        {id : 3, descricao : 'Substituto'}
+    ];
+
     self.novo = function () {
         self.habilitarModoFormulario();
         self.cte = {};
